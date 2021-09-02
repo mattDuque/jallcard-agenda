0079e2a5ddaab320ebe13a46b5292f7f81132efa
Matheus Duque <masaohc@gmail.com>
2021-09-02 20:41:31 -0300

    limpar boilerplate e preparacoes para começar

---
+++ App.js
@@ -0,0 +1,17 @@
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import './Styles/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+
+      </div>
+      <div className="app__body">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
